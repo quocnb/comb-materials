f55e5c6fc0a76ca5c8ab0f1e84e35589ca975ec1
Quoc Nguyen <quocnb.vnu@gmail.com>
2024-01-09 19:34:53 +0900

    Chapter 4: Filtering Operators

--- 04-filtering-operators/projects/Starter.playground/Contents.swift
+++ 04-filtering-operators/projects/Starter.playground/Contents.swift
@@ -3,7 +3,161 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "prefix(untilOutputFrom") {
+	let isReady = PassthroughSubject<Void, Never>()
+	
+	let taps = PassthroughSubject<Int, Never>()
+	
+	taps.prefix(untilOutputFrom: isReady).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+	
+	(1...5).forEach {
+		taps.send($0)
+		if $0 == 2 {
+			isReady.send()
+		}
+	}
+}
+
+example(of: "prefix(while:)") {
+	let number = (1...10).publisher
+	
+	number.prefix(while: { $0 < 3 }).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "prefix") {
+	let number = (1...10).publisher
+	
+	number.prefix(2).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "filter") {
+	let numbers = (1...10).publisher
+	
+	numbers.filter({
+		$0.isMultiple(of: 3)
+	}).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "Removed Duplicate") {
+	let words = "hey hey there! want to listen to mister mister ?".components(separatedBy: " ").publisher
+	words.removeDuplicates().sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "compactMap") {
+	let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
+	strings.compactMap { Float($0) }.sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "ignoreOutput") {
+	let numbers = (1...10_000).publisher
+	
+	numbers.ignoreOutput().sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+
+example(of: "first(where)") {
+	let numbers = (1...9).publisher
+	
+	numbers.print("numbers").first(where: { $0 % 2 == 0 }).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "last(where") {
+	let numbers = (1...9).publisher
+	
+	numbers.last(where: {$0 % 2 == 0}).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "last(where)") {
+	let numbers = PassthroughSubject<Int, Never>()
+	
+	numbers.last(where: { $0 % 2 == 0 }).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+	
+	numbers.send(1)
+	numbers.send(2)
+	numbers.send(3)
+	numbers.send(4)
+	numbers.send(5)
+	
+	numbers.send(completion: .finished)
+}
+
+example(of: "dropFirst") {
+	let numbers = (1...10).publisher
+	
+	numbers.dropFirst(8).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "drop(while)") {
+	let numbers = (1...10).publisher
+	
+	numbers.drop(while: { $0 % 5 != 0 }).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+}
+
+example(of: "drop(untilOutputFrom") {
+	let isReady = PassthroughSubject<Void, Never>()
+	
+	let taps = PassthroughSubject<Int, Never>()
+	taps.drop(untilOutputFrom: isReady).sink {
+		print("Received completion", $0)
+	} receiveValue: {
+		print("Received value", $0)
+	}.store(in: &subscriptions)
+	
+	(1...5).forEach {
+		taps.send($0)
+		if $0 == 3 {
+			isReady.send()
+		}
+	}
+}
 
 /// Copyright (c) 2023 Kodeco Inc.
 ///
--- 04-filtering-operators/projects/challenge/Starter.playground/Contents.swift
+++ 04-filtering-operators/projects/challenge/Starter.playground/Contents.swift
@@ -3,7 +3,8 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+let numbers = (1...100).publisher
+numbers.dropFirst(50).prefix(20).filter( {$0 % 2 == 0}).sink(receiveValue: { print($0) })
 
 /// Copyright (c) 2023 Kodeco Inc.
 ///
