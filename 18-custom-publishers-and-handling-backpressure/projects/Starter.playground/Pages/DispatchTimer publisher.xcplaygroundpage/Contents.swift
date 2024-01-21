2c80b75d538917baafadee58d7a389640da7ea49
Quoc Nguyen <quocnb.vnu@gmail.com>
2024-01-21 21:30:29 +0900

    Chapter 18 Custom Publisher and Handling Backpressure

--- 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/DispatchTimer publisher.xcplaygroundpage/Contents.swift
+++ 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/DispatchTimer publisher.xcplaygroundpage/Contents.swift
@@ -1,7 +1,101 @@
 import Foundation
 import Combine
 
-<#Add your code here#>
+struct DispatchTimerConfiguration {
+	/// The queue that the timer will fire on
+	let queue: DispatchQueue?
+	/// The interval at which the timer fires, starting from the subscription time
+	let interval: DispatchTimeInterval
+	/// The maximum of time after the deadline that the system may delay the delivery of the timer event
+	let leeway: DispatchTimeInterval
+	/// the number of events you want to receive
+	let times: Subscribers.Demand
+}
+
+extension Publishers {
+	struct DispatchTimer: Publisher {
+		typealias Output = DispatchTime
+		typealias Failure = Never
+		
+		let configuration: DispatchTimerConfiguration
+		
+		init(configuration: DispatchTimerConfiguration) {
+			self.configuration = configuration
+		}
+		
+		func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Output == S.Input {
+			let subscription = DispatchTimerSubscription(subscriber: subscriber, configuration: configuration)
+			subscriber.receive(subscription: subscription)
+		}
+	}
+}
+
+private final class DispatchTimerSubscription<S: Subscriber>: Subscription where S.Input == DispatchTime {
+	/// The configuration that the subscriber passed.
+	let configuration: DispatchTimerConfiguration
+	/// The maximum number of times the timer will fire
+	var times: Subscribers.Demand
+	/// The current demand; e.g., the number of values the subscriber requested — you decrement it every time you send a value.
+	var requested: Subscribers.Demand = .none
+	/// The internal DispatchSourceTimer that will generate the timer events.
+	var source: DispatchSourceTimer?
+	/// The subscriber. This makes it clear that the subscription is responsible for retaining the subscriber for as long as it doesn’t complete, fail or cancel.
+	var subscriber: S?
+	
+	init(subscriber: S, configuration: DispatchTimerConfiguration) {
+		self.configuration = configuration
+		self.subscriber = subscriber
+		self.times = configuration.times
+	}
+	
+	func request(_ demand: Subscribers.Demand) {
+		guard times > .none else {
+			subscriber?.receive(completion: .finished)
+			return
+		}
+		requested += demand
+		
+		if source == nil, requested > .none {
+			let source = DispatchSource.makeTimerSource(queue: configuration.queue)
+			source.schedule(deadline: .now() + configuration.interval, repeating: configuration.interval, leeway: configuration.leeway)
+			source.setEventHandler { [weak self] in
+				guard let self = self, self.requested > .none else { return }
+				self.requested -= .max(1)
+				self.times -= .max(1)
+				_ = self.subscriber?.receive(.now())
+				
+				if self.times == .none {
+					self.subscriber?.receive(completion: .finished)
+				}
+			}
+			self.source = source
+			source.activate()
+		}
+	}
+	
+	func cancel() {
+		source = nil
+		subscriber = nil
+	}
+}
+
+extension Publishers {
+	static func timer(queue: DispatchQueue? = nil, inteval: DispatchTimeInterval, leeway: DispatchTimeInterval = .nanoseconds(0), times: Subscribers.Demand = .unlimited) -> Publishers.DispatchTimer {
+		return Publishers.DispatchTimer(configuration: .init(queue: queue, interval: inteval, leeway: leeway, times: times))
+	}
+}
+
+var logger = TimeLogger(sinceOrigin: true)
+
+let publisher = Publishers.timer(inteval: .seconds(1), times: .max(6))
+
+let subscription = publisher.sink { time in
+	print("Timer emits: \(time)", to: &logger)
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+	subscription.cancel()
+}
 
 //: [Next](@next)
 /*:
--- 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
+++ 18-custom-publishers-and-handling-backpressure/projects/Starter.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
@@ -1,8 +1,205 @@
 import Foundation
 import Combine
 
-<#Add your code here#>
+fileprivate final class ShareReplaySubscription<Output, Failure: Error>: Subscription {
+	/// The replay buffer's maximum capacity
+	let capacity: Int
+	/// Keep a reference of to the subscriber for the duration of the subscription
+	var subscriber: AnySubscriber<Output, Failure>? = nil
+	/// Tracks the accumulated demands the publisher receives from the subscriber so
+	/// that you can deliver exactly the requested number of values.
+	var demand: Subscribers.Demand = .none
+	/// Stores pending values in a buffer until they are either delivered to the subscriber or thrown away.
+	var buffer: [Output]
+	/// This keeps the potential completion event around, so that it’s ready to deliver to new subscribers as soon as they begin requesting values.
+	var completion: Subscribers.Completion<Failure>? = nil
+	
+	
+	init<S>(subscriber: S, replay: [Output], capacity: Int, completion: Subscribers.Completion<Failure>?) where S: Subscriber, Failure == S.Failure, Output == S.Input {
+		self.subscriber = AnySubscriber(subscriber)
+		self.buffer = replay
+		self.capacity = capacity
+		self.completion = completion
+	}
 
+	private func complete(with completion: Subscribers.Completion<Failure>) {
+		guard let subscriber = self.subscriber else { return }
+		self.subscriber = nil
+		self.completion = nil
+		self.buffer.removeAll()
+		subscriber.receive(completion: completion)
+	}
+	
+	private func emitAsNeeded() {
+		guard let subscriber = self.subscriber else { return }
+		while self.demand > .none && !buffer.isEmpty {
+			self.demand -= .max(1)
+			let nextDemand = subscriber.receive(buffer.removeFirst())
+			if nextDemand != .none {
+				self.demand += nextDemand
+			}
+		}
+		if let completion = self.completion {
+			complete(with: completion)
+		}
+	}
+	
+	func request(_ demand: Subscribers.Demand) {
+		if demand != .none {
+			self.demand += demand
+		}
+		emitAsNeeded()
+	}
+	
+	func cancel() {
+		complete(with: .finished)
+	}
+	
+	func receive(_ input: Output) {
+		guard self.subscriber != nil else {
+			return
+		}
+		buffer.append(input)
+		if buffer.count > capacity {
+			buffer.removeFirst()
+		}
+		emitAsNeeded()
+	}
+	
+	func receive(completion: Subscribers.Completion<Failure>) {
+		guard let subscriber = self.subscriber else { return }
+		self.subscriber = nil
+		self.buffer.removeAll()
+		subscriber.receive(completion: completion)
+	}
+}
+
+extension Publishers {
+	final class ShareReplay<Upstream: Publisher>: Publisher {
+		typealias Output = Upstream.Output
+		typealias Failure = Upstream.Failure
+		
+		private let lock = NSRecursiveLock()
+		
+		private let upstream: Upstream
+		
+		private let capacity: Int
+		
+		private var replay = [Output]()
+		
+		private var subscriptions = [ShareReplaySubscription<Output, Failure>]()
+		
+		private var completion: Subscribers.Completion<Failure>? = nil
+		
+		init(upstream: Upstream, capacity: Int) {
+			self.upstream = upstream
+			self.capacity = capacity
+		}
+		
+		func receive<S>(subscriber: S) where S : Subscriber, Upstream.Failure == S.Failure, Upstream.Output == S.Input {
+			lock.lock()
+			defer {
+				lock.unlock()
+			}
+			let subscription = ShareReplaySubscription(subscriber: subscriber, replay: self.replay, capacity: self.capacity, completion: self.completion)
+			self.subscriptions.append(subscription)
+			subscriber.receive(subscription: subscription)
+			
+			guard self.subscriptions.count == 1 else {
+				return
+			}
+			
+			let sink = AnySubscriber { subscription in
+				subscription.request(.unlimited)
+			} receiveValue: { [weak self] (value: Output) in
+				self?.replay(value)
+				return .none
+			} receiveCompletion: { [weak self] completion in
+				self?.complete(completion)
+			}
+			upstream.subscribe(sink)
+		}
+		
+		private func replay(_ value: Output) {
+			lock.lock()
+			defer {
+				lock.unlock()
+			}
+			guard self.completion == nil else {
+				return
+			}
+			
+			replay.append(value)
+			if replay.count > capacity {
+				replay.removeFirst()
+			}
+			
+			self.subscriptions.forEach { subscription in
+				subscription.receive(value)
+			}
+		}
+		
+		private func complete(_ completion: Subscribers.Completion<Failure>) {
+			lock.lock()
+			defer {
+				lock.unlock()
+			}
+			self.completion = completion
+			self.subscriptions.forEach { subscription in
+				subscription.receive(completion: completion)
+			}
+		}
+	}
+}
+
+extension Publisher {
+	func shareReplay(capacity: Int = .max) -> Publishers.ShareReplay<Self> {
+		return Publishers.ShareReplay(upstream: self, capacity: capacity)
+	}
+}
+
+var logger = TimeLogger(sinceOrigin: true)
+let subject = PassthroughSubject<Int,Never>()
+let publisher = subject
+	.print("shareReplay")
+	.shareReplay(capacity: 2)
+subject.send(0)
+
+let subscription1 = publisher.sink(
+	receiveCompletion: {
+		print("subscription1 completed: \($0)", to: &logger)
+	},
+	receiveValue: {
+		print("subscription1 received \($0)", to: &logger)
+	}
+)
+subject.send(1)
+subject.send(2)
+subject.send(3)
+
+let subscription2 = publisher.sink(
+	receiveCompletion: {
+		print("subscription2 completed: \($0)", to: &logger)
+	},
+	receiveValue: {
+		print("subscription2 received \($0)", to: &logger)
+	}
+)
+subject.send(4)
+subject.send(5)
+subject.send(completion: .finished)
+
+var subscription3: Cancellable? = nil
+DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+	print("Subscribing to shareReplay after upstream completed")
+	subscription3 = publisher.sink(
+		receiveCompletion: {
+			print("subscription3 completed: \($0)", to: &logger)
+		},
+		receiveValue: {
+			print("subscription3 received \($0)", to: &logger)
+		}
+) }
 //: [Next](@next)
 /*:
  Copyright (c) 2023 Kodeco Inc.
